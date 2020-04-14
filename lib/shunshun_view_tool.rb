e8be53e5fb260f304ab19fdf2010098564a41b64
ShunshunL <shunshunlyu@protonmail.com>
2020-04-14 15:50:24 -0700

    view tool initial commit

---
+++ lib/shunshun_view_tool.rb
@@ -0,0 +1,7 @@
+require "shunshun_view_tool/version"
+require "shunshun_view_tool/renderer"
+
+module ShunshunViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/shunshun_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ShunshunViewTool
+  VERSION = "0.1.0"
+end
